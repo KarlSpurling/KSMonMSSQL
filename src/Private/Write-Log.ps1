511331c808752505fe04a78f53fdd9375a09c385
KarlSpurling <karl.spurling@outlook.com>
2026-03-16 07:03:43 +1000

    Release 2
    Write-Log is now a function

--- src/Private/Write-Log.ps1
+++ src/Private/Write-Log.ps1
@@ -1,9 +1,11 @@
-param(
-    [Parameter(Mandatory)]
-    [string]$Message,
+function Write-Log {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Message,
 
-    [string]$Path = "$PSScriptRoot\..\..\logs\module.log"
-)
+        [string]$Path = "$PSScriptRoot\..\..\logs\module.log"
+    )
 
-$Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-"$Timestamp $Message" | Out-File -FilePath $Path -Append -Encoding utf8
+    $Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$Timestamp $Message" | Out-File -FilePath $Path -Append -Encoding utf8
+}
