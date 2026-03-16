9424619a5fa19f4f706f42bef3552d30379d803f
KarlSpurling <karl.spurling@outlook.com>
2026-03-16 19:01:21 +1000

    Update Invoke-DbQuery
    Log connection stuff

--- src/Private/Invoke-DbQuery.ps1
+++ src/Private/Invoke-DbQuery.ps1
@@ -10,5 +10,27 @@ function Invoke-DbQuery {
         [string]$Query
     )
 
-    Invoke-Sqlcmd -ServerInstance $SqlInstance -Database $Database -Query $Query
+    try {
+        # Detect host,port format
+        if ($SqlInstance -match ',') {
+            $parts = $SqlInstance.Split(',')
+            $server = $parts[0]
+            $port   = $parts[1]
+
+            $conn = "Server=tcp:$server,$port;Database=$Database;Trusted_Connection=True;"
+            Write-Log "Connecting using connection string: $conn"
+
+            return Invoke-Sqlcmd -ConnectionString $conn -Query $Query -ErrorAction Stop
+        }
+        else {
+            Write-Log "Connecting using ServerInstance=$SqlInstance Database=$Database"
+            return Invoke-Sqlcmd -ServerInstance $SqlInstance -Database $Database -Query $Query -ErrorAction Stop
+        }
+    }
+    catch {
+        $msg = "SQL connection failed: $($_.Exception.Message)"
+        Write-Log $msg
+        Write-Warning $msg
+        throw
+    }
 }
